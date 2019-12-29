ca8ec7436a77b64e8b8901b2e812287a54ca6a8d
Jordan Matelsky <j6k4m8@gmail.com>
2019-12-28 19:23:46 -0500

    [code] Get members of the senate with one script

---
+++ code/get-members.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+"""
+Get a list of members of the CSPAN "Members of Congress" list.
+
+Prints to stdout.
+"""
+
+
+import tweepy
+import os
+import sys
+
+T = tweepy.API(
+    auth_handler=tweepy.auth.OAuthHandler(
+        consumer_key=os.getenv("T_CONSUMER_KEY"),
+        consumer_secret=os.getenv("T_CONSUMER_SECRET"),
+    )
+)
+
+
+def main(owner: str, slug: str):
+    congress_accts = []
+    for page in tweepy.Cursor(
+        T.list_members, owner_screen_name=owner, slug=slug
+    ).pages():
+        congress_accts.extend(page)
+
+    print("\n".join([u.screen_name for u in congress_accts]))
+
+
+if __name__ == "__main__":
+    house = sys.argv[-2].lower()
+    if house not in ["senate", "house"]:
+        raise ValueError("Invoke with get-members.py [senate|house] [R|D]")
+    party = sys.argv[-1].lower()
+    if party not in ["r", "d"]:
+        raise ValueError("Invoke with get-members.py [senate|house] [R|D]")
+
+    main(
+        *{
+            ("senate", "r"): ("SenateDems", "senaterepublicans"),
+            ("senate", "d"): ("SenateDems", "senatedemocrats"),
+        }[(house, party)]
+    )
+
