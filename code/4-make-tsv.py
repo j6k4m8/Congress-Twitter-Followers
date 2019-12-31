adbe7326ccc9a1514404bf1dcb255da774cf1668
Jordan Matelsky <j6k4m8@gmail.com>
2019-12-31 18:14:38 +0100

    Continue adding followers (133300)

--- code/4-make-tsv.py
+++ code/4-make-tsv.py
@@ -7,7 +7,7 @@ PATH = "../data/users/"
 users = glob.glob(PATH + "*.json")
 
 user_info = pd.DataFrame()
-user_info = user_info.append([json.load(open(u, 'r')) for u in users])
+user_info = user_info.append([json.load(open(u, 'r')) for u in tqdm.tqdm(users)])
 print(len(user_info))
 
 user_info.to_csv("../data/users.tsv", "\t", quoting=2)
