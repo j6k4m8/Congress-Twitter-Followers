249fec1a8e067372b4263eed7217983e4c8ad6f4
Jordan Matelsky <j6k4m8@gmail.com>
2019-12-31 19:44:29 +0100

    Remove default profile pics from tsv

--- code/4-make-tsv.py
+++ code/4-make-tsv.py
@@ -10,4 +10,8 @@ user_info = pd.DataFrame()
 user_info = user_info.append([json.load(open(u, 'r')) for u in tqdm.tqdm(users)])
 print(len(user_info))
 
+user_info.profile_image_url[
+    user_info.profile_image_url == 'https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png'
+] = None
+
 user_info.to_csv("../data/users.tsv", "\t", quoting=2)
